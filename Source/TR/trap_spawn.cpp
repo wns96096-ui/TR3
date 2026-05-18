c255723d80cff81a8c935cd9ce25cff08cf99ff2
IMBOT-INC <master@i-mbot.info>
2026-05-18 14:23:11 +0900

    trap_base 변경

--- Source/TR/trap_spawn.cpp
+++ Source/TR/trap_spawn.cpp
@@ -6,30 +6,144 @@
 
 namespace
 {
-bool IsNearExistingTrapStartLocation(
-	const FVector& SpawnLocation,
-	const TArray<Atrap_base*>& ExistingTraps,
-	float ExclusionDistance
-)
-{
-	const float ExclusionDistanceSquared = FMath::Square(ExclusionDistance);
-
-	for (const Atrap_base* ExistingTrap : ExistingTraps)
+	bool IsNearExistingTrapStartLocation(
+			const FVector& SpawnLocation,
+			const TArray<Atrap_base*>& ExistingTraps,
+			float ExclusionDistance
+			)
 	{
-		if (!IsValid(ExistingTrap))
+		const float ExclusionDistanceSquared = FMath::Square(ExclusionDistance);
+
+		for (const Atrap_base* ExistingTrap : ExistingTraps)
 		{
-			continue;
+			if (!IsValid(ExistingTrap))
+			{
+				continue;
+			}
+
+			if (FVector::DistSquared2D(SpawnLocation,
+			                           ExistingTrap->GetInitialSpawnLocation())
+				<= ExclusionDistanceSquared)
+			{
+				return true;
+			}
 		}
 
-		if (FVector::DistSquared2D(SpawnLocation, ExistingTrap->GetInitialSpawnLocation()) <= ExclusionDistanceSquared)
+		return false;
+	}
+}
+
+TArray<Atrap_base*> Utrap_spawn::SpawnTrap(
+		AActor* OriginActor,
+		TSubclassOf<Atrap_base> TrapClass,
+		// TrapType TrapType,
+		// // const TArray<Atrap_base*>& ExistingTraps,
+		// Utrap_BP_assets* TrapAssets,
+		float ExclusionDistance,
+		float YOffset,
+		float XStart,
+		float XEnd,
+		int32 SlotCount,
+		int32 SpawnCount
+		)
+{
+	TArray<Atrap_base*> SpawnedTraps;
+
+	if (!OriginActor || !TrapClass || SlotCount <= 0 || SpawnCount <= 0)
+	{
+		return SpawnedTraps;
+	}
+
+	UWorld* World = OriginActor->GetWorld();
+	if (!World)
+	{
+		return SpawnedTraps;
+	}
+	//트랩 Assets 제거	
+	// TSubclassOf<Atrap_base> TrapClass = TrapAssets->GetTrapClass(TrapType);
+	// if (!TrapClass)
+	// {
+	// 	return SpawnedTraps;
+	// }  
+
+	const int32 TargetSpawnCount = FMath::Min(SpawnCount, SlotCount);
+	SpawnedTraps.Reserve(TargetSpawnCount);
+
+	TArray<int32> SlotIndices;
+	SlotIndices.Reserve(SlotCount);
+	for (int32 SlotIndex = 0; SlotIndex < SlotCount; ++SlotIndex)
+	{
+		SlotIndices.Add(SlotIndex);
+	}
+
+	for (int32 Index = SlotIndices.Num() - 1; Index > 0; --Index)
+	{
+		const int32 SwapIndex = FMath::RandRange(0, Index);
+		SlotIndices.Swap(Index, SwapIndex);
+	}
+
+	const FVector OriginLocation = OriginActor->GetActorLocation();
+	const FRotator SpawnRotation = OriginActor->GetActorRotation();
+	const float XStep = SlotCount > 1
+		                    ? (XEnd - XStart) / static_cast<float>(SlotCount -
+			                    1)
+		                    : 0.0f;
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = OriginActor;
+	SpawnParams.Instigator = OriginActor->GetInstigator();
+	SpawnParams.SpawnCollisionHandlingOverride =
+			ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+	for (const int32 SlotIndex : SlotIndices)
+	{
+		if (SpawnedTraps.Num() >= TargetSpawnCount)
 		{
-			return true;
+			break;
+		}
+
+		const float XOffset = XStart + (XStep * SlotIndex);
+		const FVector SpawnLocation = FVector(
+				XOffset,
+				OriginLocation.Y + YOffset,
+				0.0f
+				);
+		// TODO 그 뭐냐 가까운거 지우기
+		// if (ExclusionDistance > 0.0f && IsNearExistingTrapStartLocation(SpawnLocation, ExistingTraps, ExclusionDistance))
+		// { 
+		// 	continue;
+		// }
+		
+		
+		//TODO 생성
+		Atrap_base* SpawnedTrap = World->SpawnActor<Atrap_base>(
+				TrapClass,
+				SpawnLocation,
+				SpawnRotation,
+				SpawnParams
+				);
+
+		if (SpawnedTrap)
+		{
+			SpawnedTraps.Add(SpawnedTrap);
 		}
 	}
 
-	return false;
+	return SpawnedTraps;
 }
+
+void Utrap_spawn::DestroySpawnedTraps(TArray<Atrap_base*>& Traps)
+{
+	for (Atrap_base* Trap : Traps)
+	{
+		if (IsValid(Trap))
+		{
+			Trap->Destroy();
+		}
+	}
+
+	Traps.Empty();
 }
+
 //
 // TArray<Atrap_base*> Utrap_spawn::SpawnTest1(
 // 	AActor* OriginActor,
@@ -166,108 +280,3 @@ bool IsNearExistingTrapStartLocation(
 //
 // 	return SpawnedTraps;
 // }
-
-TArray<Atrap_base*> Utrap_spawn::SpawnTrap(
-	AActor* OriginActor,
-	Utrap_BP_assets* TrapAssets,
-	TrapType TrapType,
-	const TArray<Atrap_base*>& ExistingTraps,
-	float ExclusionDistance,
-	float YOffset,
-	float XStart,
-	float XEnd,
-	int32 SlotCount,
-	int32 SpawnCount
-)
-{
-	TArray<Atrap_base*> SpawnedTraps;
-
-	if (!OriginActor || !TrapAssets || SlotCount <= 0 || SpawnCount <= 0)
-	{
-		return SpawnedTraps;
-	}
-
-	UWorld* World = OriginActor->GetWorld();
-	if (!World)
-	{
-		return SpawnedTraps;
-	}
-
-	TSubclassOf<Atrap_base> TrapClass = TrapAssets->GetTrapClass(TrapType);
-	if (!TrapClass)
-	{
-		return SpawnedTraps;
-	}
-
-	const int32 TargetSpawnCount = FMath::Min(SpawnCount, SlotCount);
-	SpawnedTraps.Reserve(TargetSpawnCount);
-
-	TArray<int32> SlotIndices;
-	SlotIndices.Reserve(SlotCount);
-	for (int32 SlotIndex = 0; SlotIndex < SlotCount; ++SlotIndex)
-	{
-		SlotIndices.Add(SlotIndex);
-	}
-
-	for (int32 Index = SlotIndices.Num() - 1; Index > 0; --Index)
-	{
-		const int32 SwapIndex = FMath::RandRange(0, Index);
-		SlotIndices.Swap(Index, SwapIndex);
-	}
-
-	const FVector OriginLocation = OriginActor->GetActorLocation();
-	const FRotator SpawnRotation = OriginActor->GetActorRotation();
-	const float XStep = SlotCount > 1 ? (XEnd - XStart) / static_cast<float>(SlotCount - 1) : 0.0f;
-
-	FActorSpawnParameters SpawnParams;
-	SpawnParams.Owner = OriginActor;
-	SpawnParams.Instigator = OriginActor->GetInstigator();
-	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
-
-	for (const int32 SlotIndex : SlotIndices)
-	{
-		if (SpawnedTraps.Num() >= TargetSpawnCount)
-		{
-			break;
-		}
-
-		const float XOffset = XStart + (XStep * SlotIndex);
-		const FVector SpawnLocation = FVector(
-			XOffset,
-			OriginLocation.Y + YOffset,
-			0.0f
-		);
-
-		if (ExclusionDistance > 0.0f && IsNearExistingTrapStartLocation(SpawnLocation, ExistingTraps, ExclusionDistance))
-		{
-			continue;
-		}
-
-		Atrap_base* SpawnedTrap = World->SpawnActor<Atrap_base>(
-			TrapClass,
-			SpawnLocation,
-			SpawnRotation,
-			SpawnParams
-		);
-
-		if (SpawnedTrap)
-		{
-			SpawnedTraps.Add(SpawnedTrap);
-		}
-	}
-
-	return SpawnedTraps;
-}
-
-void Utrap_spawn::DestroySpawnedTraps(TArray<Atrap_base*>& Traps)
-{
-	for (Atrap_base* Trap : Traps)
-	{
-		if (IsValid(Trap))
-		{
-			Trap->Destroy();
-		}
-	}
-
-	Traps.Empty();
-}
